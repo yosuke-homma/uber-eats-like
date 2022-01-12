b1e356a8dff960c9c9b235102c870cd99225d54a
yosuke-homma <y.homma0708@gmail.com>
2022-01-12 22:00:52 +0900

    Revert "Feature/foods controller"

--- app/models/food.rb
+++
@@ -1,5 +0,0 @@
-class Food < ApplicationRecord
-  belongs_to :restaurant
-  belongs_to :order, optional: true
-  has_one :line_food
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,3 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      resources :restaurants do
-        resources :foods, only: %i[index]
-      end
-      resources :line_foods, only: %i[index create]
-      put 'line_foods/replace', to: 'line_foods#replace'
-      resources :orders, only: %i[create]
-    end
-  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
