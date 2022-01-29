0fdc39bc9289cafb882f41e3d229fc40adf19a34
yosuke-homma <y.homma0708@gmail.com>
2022-01-29 16:31:09 +0900

    Add functions to call linefoods API

--- frontend/src/containers/Orders.jsx
+++ frontend/src/containers/Orders.jsx
@@ -1,6 +1,31 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useReducer } from 'react';
+import { fetchLineFoods } from '../apis/line_foods';
+
+// reducers
+import {
+  initialState,
+  lineFoodsActionTypes,
+  lineFoodsReducer,
+} from '../reducers/lineFoods';
 
 export const Orders = () => {
+
+  const [state, dispatch] = useReducer(lineFoodsReducer, initialState);
+
+  useEffect(() => {
+    dispatch({ type: lineFoodsActionTypes.FETCHING });
+    fetchLineFoods()
+      .then((data) =>
+        dispatch({
+          type: lineFoodsActionTypes.FETCH_SUCCESS,
+          payload: {
+            lineFoodsSummary: data
+          }
+        })
+      )
+      .catch((e) => console.log(e));
+  }, []);
+
   return (
     <Fragment>
       注文画面
